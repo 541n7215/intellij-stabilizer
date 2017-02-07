164ac99ab5a9246d464cec0ce1844b1f3ce4535e
Bob Miller <bobprime@bobprime.net>
2017-02-06 21:50:32 -0500

    Initial commit

---
+++ src/main/groovy/com/shysteph/plugin/IntellijStabilizer.groovy
@@ -0,0 +1,14 @@
+package com.shysteph.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by bobprime on 2017-02-06.
+ */
+class IntellijStabilizer implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
